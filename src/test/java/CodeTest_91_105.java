048b962ab1c4db925c99b63995f9bbe35d5892fc
songshu198907 <songshu198907@gmail.com>
2016-05-04 22:57:07 -0400

    Check in code

---
+++ src/test/java/CodeTest_91_105.java
@@ -0,0 +1,13 @@
+import algorithm.leet_91_105.DecodeWays_91;
+import org.junit.Test;
+
+/**
+ * Created by shu on 5/4/2016.
+ */
+public class CodeTest_91_105 {
+    @Test
+    public void test91() {
+        DecodeWays_91 decode = new DecodeWays_91();
+        System.out.println(decode.numDecodings("123545612"));
+    }
+}
