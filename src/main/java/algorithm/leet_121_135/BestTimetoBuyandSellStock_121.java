6dab65128c1740de3b8ca5bfce49e630f3efa856
songshu198907 <songshu198907@gmail.com>
2016-05-10 17:14:13 -0400

    add tests

---
+++ src/main/java/algorithm/leet_121_135/BestTimetoBuyandSellStock_121.java
@@ -0,0 +1,23 @@
+package algorithm.leet_121_135;
+
+/**
+ * Created by songheng on 5/10/16.
+ */
+public class BestTimetoBuyandSellStock_121 {
+    public int maxProfit(int[] prices) {
+        if(prices == null || prices.length == 1 || prices.length ==0){
+            return 0;
+        }
+        int[] left = new int[prices.length + 1];
+        left[0] = 0;
+        int res = Integer.MIN_VALUE;
+        for (int i = 1; i < prices.length; i++) {
+            left[i] = Math.min(left[i - 1], prices[i - 1]);
+            int profit  = prices[i] - left[i];
+            if(profit > res){
+                res = profit;
+            }
+        }
+        return res;
+    }
+}
---
+++ src/test/java/CodeTest_121_135.java
@@ -0,0 +1,5 @@
+/**
+ * Created by songheng on 5/10/16.
+ */
+public class CodeTest_121_135 {
+}
